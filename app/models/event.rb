bd07fcbe2b9c78ff656890a70207dde685eb29c2
Chris Metzger <36425627+critsmet@users.noreply.github.com>
2019-06-14 15:10:32 -0400

    Update event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,6 @@ end
 
 # Event.all
   # Returns an array of all Events
-# Event#sales_to_break_even
-  # Returns the number of Tickets that still need to be "purchased" for this Event before the Event reaches the break-even point.
-  # If the Event has sold enough Tickets to break-even, return the number 0. 
 # Event#attendees
   # Returns an array of Attendee that have Tickets for the Event
 # Event#average_age
